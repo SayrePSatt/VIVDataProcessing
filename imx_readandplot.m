3fe553188a28a28ab16017e1b7e1b179924dfe32
SayrePSatt <sayresatterwhite@gmail.com>
2026-02-26 18:05:19 -0500

    Implemented contour plots for velocity

--- imx_readandplot.m
+++ imx_readandplot.m
@@ -4,7 +4,7 @@ clc
 
 load("pumpFit_freq2velo.mat");
 
-topfolder = 'E:\EFDL\NEW_VIVmaster\tandemSphereData\velo_fields\02_24_2026\outoffoler\';
+topfolder = 'D:\EFDL\NEW_VIVmaster\tandemSphereData\velo_fields\02_24_2026\outoffoler\';
 [processing_files, processing_file_dir] = uigetfile(topfolder,'*.vc7','MultiSelect','on');
 
 tic 
@@ -45,9 +45,11 @@ colors = [
 colors = flipud(colors);
 cmap = colors;
 %%
-max_vorticity = 3;
-smoothing_window = 7;
+max_vorticity = 1;
+smoothing_window = 3; %7 for vorticity
 N=2;
+value_to_plot = 4; %1 for vorticity, 2 for u, 3 for v, 4 for w
+sliding_ave = ones(smoothing_window,smoothing_window)/smoothing_window^2;
 
 for ii=1:length(processing_files)
 close all
@@ -81,75 +83,100 @@ X = D.X(:,1)/1000;
 Y = D.Y(1,:)/1000;
 [XX, YY] = meshgrid(X,Y);
 
-vorticity = curl(X,Y,D.U',D.V');
-vorticityStar = vorticity*diameter/U;
-
-
-u = vorticityStar;
-sliding_ave = ones(smoothing_window,smoothing_window)/smoothing_window^2;
-u = conv2(vorticityStar,sliding_ave,'same');
-max(abs(u),[],"all")
-% 
-
-% alpha = 0.1;           % Filter strength; try between 0.01 and 0.2 for tuning
-% nIterations = 3;      % How many times to apply filter? (e.g., 5-50)
-% 
-% for k = 1:nIterations
-%     % Compute discrete Laplacian
-%     laplacian = -4*u + ...
-%                 circshift(u, [1, 0]) + ... % up
-%                 circshift(u, [-1, 0]) + ... % down
-%                 circshift(u, [0, 1]) + ... % right
-%                 circshift(u, [0, -1]);     % left
-% 
-%     % Update with Laplacian (diffusion smoothing)
-%     u = u + alpha * laplacian;
-% end
-% % sigma = 3; % Standard deviation for the Gaussian filter
-% % h_gaussian = fspecial('gaussian', [25 25], sigma);
-% % u = imfilter(vorticityStar, h_gaussian, 'replicate');
-% 
-% % [startX,startY] = meshgrid(-0.1:0.01:0.05,-0.06:0.01:0.06);
-
-skip = 4;
-% f = figure('units','inch','position',[1,1,8,6]);
-% ax = gca;
-% ax.LineWidth = 4;
-% ax.FontSize = 18;
-% ax.FontWeight = 'bold';
-hold on
-
 Nx = N*size(X,1);
 Ny = N*size(Y,2);
 [XXq, YYq] = meshgrid(linspace(min(X),max(X),Nx), linspace(min(Y),max(Y),Ny)); % N larger than original size
-uq = interp2(XX, YY, u, XXq, YYq, 'cubic');
-uq(abs(uq)<0.2) = 0;
+
+if value_to_plot == 1    
+    vorticity = curl(X,Y,D.U',D.V');
+    vorticityStar = vorticity*diameter/U;
+    
+    
+    plot_parameter = vorticityStar;
+    plot_parameter = conv2(vorticityStar,sliding_ave,'same');
+    max(abs(plot_parameter),[],"all")
+    % 
+    
+    % alpha = 0.1;           % Filter strength; try between 0.01 and 0.2 for tuning
+    % nIterations = 3;      % How many times to apply filter? (e.g., 5-50)
+    % 
+    % for k = 1:nIterations
+    %     % Compute discrete Laplacian
+    %     laplacian = -4*u + ...
+    %                 circshift(u, [1, 0]) + ... % up
+    %                 circshift(u, [-1, 0]) + ... % down
+    %                 circshift(u, [0, 1]) + ... % right
+    %                 circshift(u, [0, -1]);     % left
+    % 
+    %     % Update with Laplacian (diffusion smoothing)
+    %     u = u + alpha * laplacian;
+    % end
+    % % sigma = 3; % Standard deviation for the Gaussian filter
+    % % h_gaussian = fspecial('gaussian', [25 25], sigma);
+    % % u = imfilter(vorticityStar, h_gaussian, 'replicate');
+    % 
+    % % [startX,startY] = meshgrid(-0.1:0.01:0.05,-0.06:0.01:0.06);
+    
+    skip = 4;
+    % f = figure('units','inch','position',[1,1,8,6]);
+    % ax = gca;
+    % ax.LineWidth = 4;
+    % ax.FontSize = 18;
+    % ax.FontWeight = 'bold';
+    hold on
+
+    plot_para_smooth = interp2(XX, YY, plot_parameter, XXq, YYq, 'cubic');
+    plot_para_smooth(abs(plot_para_smooth)<0.2) = 0;
+    
+    % levels = [-3 -2.1429 -1.2677 -1.2677 2.1429 3];
+    plot_para_smooth(plot_para_smooth>=max_vorticity) = max_vorticity;
+    plot_para_smooth(plot_para_smooth<=-max_vorticity) = -max_vorticity;
+    BW = abs(plot_para_smooth) > 0.3;
+    CC = bwconncomp(BW); 
+    region_area = cellfun(@numel, CC.PixelIdxList);  % Number of pixels (area) for each region
+    
+    % Sort regions by area descending
+    [~, sort_idx] = sort(region_area, 'descend');
+    
+    % Create mask for the two largest regions
+    mask = zeros(size(BW));  % Initialize mask
+    numRegionsToKeep = min(2, length(sort_idx));  % In case fewer than 2 regions exist
+    
+    for k = 1:numRegionsToKeep
+        mask(CC.PixelIdxList{sort_idx(k)}) = 1;  % Set pixels for top regions to 1
+    end
+    
+    % Keep only the two largest vortices field
+    plot_para_smooth = plot_para_smooth .* mask;
+    
+    cbar_name = '$\omega^*$';
+    plot_name = '_vorticity';
+elseif value_to_plot == 2
+    plot_parameter = D.U'/U;
+    cbar_name = '$u/U_{\infty}$';
+    plot_name = '_u';
+    plot_parameter = conv2(plot_parameter,sliding_ave,'same');
+    plot_para_smooth = interp2(XX, YY, plot_parameter, XXq, YYq, 'cubic');
+elseif value_to_plot == 3
+    plot_parameter = D.V'/U;
+    cbar_name = '$v/U_{\infty}$';
+    plot_name = '_v';
+    plot_parameter = conv2(plot_parameter,sliding_ave,'same');
+    plot_para_smooth = interp2(XX, YY, plot_parameter, XXq, YYq, 'cubic');
+elseif value_to_plot == 4
+    plot_parameter = D.W'*2/(100*U); %/100 because for some reason thru plane velocity reported in cm/s
+    cbar_name = '$w/U_{\infty}$';
+    plot_name = '_w';
+    plot_parameter = conv2(plot_parameter,sliding_ave,'same');
+    plot_para_smooth = interp2(XX, YY, plot_parameter, XXq, YYq, 'cubic');
+end
+
 levels = [-max_vorticity:2*max_vorticity/7:max_vorticity];
 levels(4:5) = [];
 levels = linspace(-max_vorticity,max_vorticity,length(colors));
-% levels = [-3 -2.1429 -1.2677 -1.2677 2.1429 3];
-uq(uq>=max_vorticity) = max_vorticity;
-uq(uq<=-max_vorticity) = -max_vorticity;
-BW = abs(uq) > 0.3;
-CC = bwconncomp(BW); 
-region_area = cellfun(@numel, CC.PixelIdxList);  % Number of pixels (area) for each region
-
-% Sort regions by area descending
-[~, sort_idx] = sort(region_area, 'descend');
-
-% Create mask for the two largest regions
-mask = zeros(size(BW));  % Initialize mask
-numRegionsToKeep = min(2, length(sort_idx));  % In case fewer than 2 regions exist
-
-for k = 1:numRegionsToKeep
-    mask(CC.PixelIdxList{sort_idx(k)}) = 1;  % Set pixels for top regions to 1
-end
-
-% Keep only the two largest vortices field
-uq = uq .* mask;
 
 % contourf(X/diameter,Y/diameter,u,levels,'LineColor','none','edgecolor','k')%,'LevelList',-max_vorticity:max_vorticity/7:max_vorticity)
-contourf(XXq/diameter,YYq/diameter,uq,levels,'LineColor','none','edgecolor','k')%,'LevelList',-max_vorticity:max_vorticity/7:max_vorticity)
+contourf(XXq/diameter,YYq/diameter,plot_para_smooth,levels,'LineColor','none','edgecolor','k')%,'LevelList',-max_vorticity:max_vorticity/7:max_vorticity)
 clim([-max_vorticity max_vorticity])
 colormap(cmap)
 % quiver(X(1:skip:end)/chord,Y(1:skip:end)/chord,ux(1:skip:end,1:skip:end),uy(1:skip:end,1:skip:end),1,'k','LineWidth',1)
@@ -271,7 +298,7 @@ h = colorbar;
 drawnow
 h.TickLabelInterpreter = 'latex';
 h.Label.Interpreter = 'latex';
-h.Label.String = '$\omega^*$';
+h.Label.String = cbar_name;
 h.Label.Rotation = 0;
 h.Location = 'eastoutside';
 h.FontWeight = 'normal';
@@ -288,7 +315,7 @@ end
 
 title(['$U^*=$' num2str(str2double(u_star)) dist_disp])
 
-exportgraphics(f,fullfile([processing_file_dir '\figures\'], [processing_files{ii}(1:end-4) '_vorticity.pdf']),'Resolution',600);
-exportgraphics(f,fullfile([processing_file_dir '\figures\'], [processing_files{ii}(1:end-4) '_vorticity.png']),'Resolution',300);
+exportgraphics(f,fullfile([processing_file_dir '\figures\'], [processing_files{ii}(1:end-4) plot_name '.pdf']),'Resolution',600);
+exportgraphics(f,fullfile([processing_file_dir '\figures\'], [processing_files{ii}(1:end-4) plot_name '.png']),'Resolution',300);
 
 end
